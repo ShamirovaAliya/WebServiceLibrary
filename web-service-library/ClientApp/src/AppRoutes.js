f62524cdd33a3e4f7671760e7d6a8f925a5a30e4
Алия Шамирова <Shamirowaaliam@gmail.com>
2022-10-27 16:27:49 +0300

    Создание меню для Администратора

--- web-service-library/ClientApp/src/AppRoutes.js
+++ web-service-library/ClientApp/src/AppRoutes.js
@@ -1,20 +1,15 @@
-﻿import { Counter } from "./components/Counter";
-import { FetchData } from "./components/FetchData";
-import { Home } from "./components/Home";
+﻿import Users from "./components/Administrator/Users";
+import AddUser from "./components/Administrator/AddUser";
 
 const AppRoutes = [
-  {
-    index: true,
-    element: <Home />
-  },
-  {
-    path: '/counter',
-    element: <Counter />
-  },
-  {
-    path: '/fetch-data',
-    element: <FetchData />
-  }
+    {
+        index: true,
+        element: <Users />
+    },
+    {
+        path: '/AddUser',
+        element: <AddUser />
+    }
 ];
 
 export default AppRoutes;
--- web-service-library/ClientApp/src/components/Layout.js
+++ web-service-library/ClientApp/src/components/Layout.js
@@ -1,18 +1,34 @@
-import React, { Component } from 'react';
-import { Container } from 'reactstrap';
+﻿import React, { Component } from 'react';
 import { NavMenu } from './NavMenu';
+import { Container } from 'reactstrap';
 
 export class Layout extends Component {
-  static displayName = Layout.name;
 
-  render() {
-    return (
-      <div>
-        <NavMenu />
-        <Container>
-          {this.props.children}
-        </Container>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.adminRoutes = [
+            {
+                routeValue: '/',
+                routeName: 'Пользователи'
+            },
+            {
+                routeValue: '/AddUser',
+                routeName: 'Добавить пользователей'
+            }
+        ];
+    }
+
+    static displayName = Layout.name;
+
+    render() {
+        return (
+            <div>
+                <NavMenu routes={this.adminRoutes} />
+                <Container>
+                    {this.props.children}
+                </Container>
+            </div>
+        );
+    }
 }
